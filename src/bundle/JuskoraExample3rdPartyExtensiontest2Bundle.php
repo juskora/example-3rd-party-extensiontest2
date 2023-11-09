be56d9bb45c5090a0ab1b50556178e27669368ba
GitHub Action <action@github.com>
2023-11-09 10:33:46 +0000

    Template cleanup

---
+++ src/bundle/JuskoraExample3rdPartyExtensiontest2Bundle.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @copyright Copyright (C) Ibexa AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Juskora\Bundle\Example3rdPartyExtensiontest2;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class JuskoraExample3rdPartyExtensiontest2Bundle extends Bundle
+{
+}
